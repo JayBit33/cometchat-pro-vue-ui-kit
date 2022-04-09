584f2e2db68b5d9a1815e02dfebe593cbe1b8f97
Jay Roseman <jaybit33@gmail.com>
2022-04-09 15:35:21 -0400

    updates to cometchat-pro-vue-ui-kit

--- CometChatWorkspace/src/components/CometChatUI/CometChatUI.vue
+++ CometChatWorkspace/src/components/CometChatUI/CometChatUI.vue
@@ -30,7 +30,7 @@
       />
     </div>
     <div v-else :style="styles.placeholder">
-      <!-- {{ placeholderMessage }} -->
+      {{ placeholderMessage }}
     </div>
     <div
       v-if="viewDetailScreen"
@@ -89,25 +89,25 @@
 import {
   COMETCHAT_CONSTANTS,
   DEFAULT_OBJECT_PROP,
-} from '../../resources/constants'
+} from "../../resources/constants";
 
-import { cometChatScreens } from '../../mixins/'
-import { CometChatManager } from '../../util/controller'
+import { cometChatScreens } from "../../mixins/";
+import { CometChatManager } from "../../util/controller";
 
 import {
   CometChatMessages,
   CometChatImageViewer,
   CometChatMessageThread,
-} from '../Messages/'
-import { CometChatUserDetails } from '../Users'
-import { CometChatGroupDetails } from '../Groups'
-import { CometChatIncomingCall, CometChatOutgoingCall } from '../Calls'
+} from "../Messages/";
+import { CometChatUserDetails } from "../Users";
+import { CometChatGroupDetails } from "../Groups";
+import { CometChatIncomingCall, CometChatOutgoingCall } from "../Calls";
 
-import CometChatNavBar from './CometChatNavBar/CometChatNavBar'
+import CometChatNavBar from "./CometChatNavBar/CometChatNavBar";
 
-import { theme } from '../../resources/theme'
+import { theme } from "../../resources/theme";
 
-import * as style from './style'
+import * as style from "./style";
 
 /**
  * Displays a fully working chat application.
@@ -115,7 +115,7 @@ import * as style from './style'
  * @displayName CometChatUI
  */
 export default {
-  name: 'CometChatUI',
+  name: "CometChatUI",
   mixins: [cometChatScreens],
   components: {
     CometChatNavBar,
@@ -136,7 +136,7 @@ export default {
   data() {
     return {
       item: {},
-      type: 'user',
+      type: "user",
       imageView: null,
       darkTheme: false,
       lastMessage: {},
@@ -149,7 +149,7 @@ export default {
       incomingCall: null,
       outgoingCall: null,
       loggedInUser: null,
-      tab: 'conversations',
+      tab: "conversations",
       messageToMarkRead: {},
       threadMessageItem: {},
       actionFromListener: {},
@@ -158,7 +158,7 @@ export default {
       threadMessageParent: {},
       viewThreadMessage: false,
       composedThreadMessage: {},
-    }
+    };
   },
   computed: {
     /**
@@ -177,30 +177,30 @@ export default {
         root: style.unifiedStyle(this.themeValue),
         placeholder: style.unifiedPlaceholderStyle(this.themeValue),
         sidebar: style.unifiedSidebarStyle(this.themeValue, this.viewSidebar),
-      }
+      };
     },
     /**
      * Theme computed using default theme and theme coming from prop.
      */
 
     themeValue() {
-      return Object.assign({}, theme, this.theme)
+      return Object.assign({}, theme, this.theme);
     },
     /**
      * Placeholder message when chat item is not selected.
      */
     placeholderMessage() {
       return this.STRINGS[
-        this.tab === 'info'
-          ? 'NO_TAB_SELECTED_MESSAGE'
-          : 'NO_ITEM_SELECTED_MESSAGE'
-      ]
+        this.tab === "info"
+          ? "NO_TAB_SELECTED_MESSAGE"
+          : "NO_ITEM_SELECTED_MESSAGE"
+      ];
     },
     /**
      * Local string constants.
      */
     STRINGS() {
-      return COMETCHAT_CONSTANTS
+      return COMETCHAT_CONSTANTS;
     },
   },
   methods: {
@@ -222,147 +222,138 @@ export default {
       rejectedCall,
     }) {
       switch (action) {
-        case 'item-click':
-          this.itemClicked(item, type)
-          break
-        case 'tabChanged':
-          this.item = {}
-          this.changeTab(tab)
-          break
-        case 'blockUser':
-          this.blockUser()
-          break
-        case 'unblockUser':
-          this.unblockUser()
-          break
-        case 'audioCall':
-          this.audioCall()
-          break
-        case 'videoCall':
-          this.videoCall()
-          break
-        case 'viewDetail':
-        case 'closeDetailClicked':
-          this.toggleDetailView()
-          break
-        case 'menuClicked':
-          this.item = {}
-          this.toggleSideBar()
-          break
-        case 'closeMenuClicked':
-          this.toggleSideBar()
-          break
-        case 'groupUpdated':
-          this.groupUpdated(item, count)
-          break
-        case 'groupDeleted':
-          this.deleteGroup(group)
-          break
-        case 'leftGroup':
-          this.leaveGroup(group)
-          break
-        case 'membersUpdated':
-          this.updateMembersCount(count)
-          break
-        case 'viewMessageThread':
-          this.viewMessageThread(message)
-          break
-        case 'closeThreadClicked':
-          this.closeThreadMessages()
-          break
-        case 'threadMessageComposed':
-          this.onThreadMessageComposed(messages)
-          this.updateLastMessage(messages[0])
-          break
-        case 'acceptIncomingCall':
-          this.acceptIncomingCall(incomingCall)
-          break
-        case 'acceptedIncomingCall':
-          this.callInitiated(call)
-          break
-        case 'rejectedIncomingCall':
-          this.rejectedIncomingCall(incomingCall, rejectedCall)
-          break
-        case 'outgoingCallRejected':
-        case 'outgoingCallCancelled':
-        case 'callEnded':
-          this.outgoingCallEnded(call)
-          break
-        case 'userJoinedCall':
-        case 'userLeftCall':
-          this.appendCallMessage(call)
-          break
-        case 'viewActualImage':
-          this.setImageView(message)
-          break
-        case 'closeActualImage':
-          this.setImageView(null)
-          break
-        case 'membersAdded':
-          this.membersAdded(members, message)
-          break
-        case 'memberUnbanned':
-          this.memberUnbanned(members, message)
-          break
-        case 'memberScopeChanged':
-          this.memberScopeChanged(members, message)
-          break
-        case 'messageDeleted':
-          this.closeThreadMessages()
-          break
-        case 'messageComposed':
-        case 'lastMessageEdited':
-        case 'lastMessageDeleted':
-          this.updateLastMessage(messages[0])
-          break
-        case 'messageEdited':
-          this.updateThreadMessage(message)
-          break
-        case 'listenerData':
-          this.actionFromListener = { action: action, messages: [...messages] }
-          break
+        case "item-click":
+          this.itemClicked(item, type);
+          break;
+        case "tabChanged":
+          this.item = {};
+          this.changeTab(tab);
+          break;
+        case "blockUser":
+          this.blockUser();
+          break;
+        case "unblockUser":
+          this.unblockUser();
+          break;
+        case "audioCall":
+          this.audioCall();
+          break;
+        case "videoCall":
+          this.videoCall();
+          break;
+        case "viewDetail":
+        case "closeDetailClicked":
+          this.toggleDetailView();
+          break;
+        case "menuClicked":
+          this.item = {};
+          this.toggleSideBar();
+          break;
+        case "closeMenuClicked":
+          this.toggleSideBar();
+          break;
+        case "groupUpdated":
+          this.groupUpdated(item, count);
+          break;
+        case "groupDeleted":
+          this.deleteGroup(group);
+          break;
+        case "leftGroup":
+          this.leaveGroup(group);
+          break;
+        case "membersUpdated":
+          this.updateMembersCount(count);
+          break;
+        case "viewMessageThread":
+          this.viewMessageThread(message);
+          break;
+        case "closeThreadClicked":
+          this.closeThreadMessages();
+          break;
+        case "threadMessageComposed":
+          this.onThreadMessageComposed(messages);
+          this.updateLastMessage(messages[0]);
+          break;
+        case "acceptIncomingCall":
+          this.acceptIncomingCall(incomingCall);
+          break;
+        case "acceptedIncomingCall":
+          this.callInitiated(call);
+          break;
+        case "rejectedIncomingCall":
+          this.rejectedIncomingCall(incomingCall, rejectedCall);
+          break;
+        case "outgoingCallRejected":
+        case "outgoingCallCancelled":
+        case "callEnded":
+          this.outgoingCallEnded(call);
+          break;
+        case "userJoinedCall":
+        case "userLeftCall":
+          this.appendCallMessage(call);
+          break;
+        case "viewActualImage":
+          this.setImageView(message);
+          break;
+        case "closeActualImage":
+          this.setImageView(null);
+          break;
+        case "membersAdded":
+          this.membersAdded(members, message);
+          break;
+        case "memberUnbanned":
+          this.memberUnbanned(members, message);
+          break;
+        case "memberScopeChanged":
+          this.memberScopeChanged(members, message);
+          break;
+        case "messageDeleted":
+          this.closeThreadMessages();
+          break;
+        case "messageComposed":
+        case "lastMessageEdited":
+        case "lastMessageDeleted":
+          this.updateLastMessage(messages[0]);
+          break;
+        case "messageEdited":
+          this.updateThreadMessage(message);
+          break;
+        case "listenerData":
+          this.actionFromListener = { action: action, messages: [...messages] };
+          break;
         default:
-          break
+          break;
       }
     },
     /**
      * Changes tab
      */
     changeTab(tab) {
-      this.tab = tab
-      this.imageView = null
-      this.viewDetailScreen = false
-      this.viewThreadMessage = false
+      this.tab = tab;
+      this.imageView = null;
+      this.viewDetailScreen = false;
+      this.viewThreadMessage = false;
     },
   },
   beforeMount() {
     if (!Object.keys(this.item).length) {
-      this.toggleSideBar()
+      this.toggleSideBar();
     }
 
-    ;(async () => {
+    (async () => {
       try {
-        this.loggedInUser = await new CometChatManager().getLoggedInUser()
+        this.loggedInUser = await new CometChatManager().getLoggedInUser();
       } catch (error) {
-        this.logError('[CometChatUnified] getLoggedInUser error', error)
+        this.logError("[CometChatUnified] getLoggedInUser error", error);
       }
-    })()
+    })();
   },
-}
+};
 </script>
 <style scoped>
 .cometchat__unified {
   box-sizing: border-box !important;
   font-family: var(--cometchat-unified-font-family) !important;
-}
-.sidebar__footer {
-  margin-top: 100%;
-}
-.unified__sidebar {
-  min-width: 400px;
-}
-.cometchat__unified__navbar {
-  width: 380px;
 }
 @media (min-width: 320px) and (max-width: 767px) {
   .unified__sidebar {
